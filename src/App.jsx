47bc59de15456dde7a1bf84a171b5ed422560171
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2024-03-25 21:23:23 +0500

    mutation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useGetPostsQuery } from './redux/api'
+import { useGetPostsQuery, useNewPostMutation } from './redux/api'
 
 import { PostItemList } from './component/PostItemList.jsx'
 function App() {
-  const [count, setCount] = useState(0)
+  const [body, setBody] = useState("")
+  const [title, setTitle] = useState("")
+
+
+
 const {isLoading,isError,isSuccess,data,error} =useGetPostsQuery("")
-console.log(data)
+
+const [newPost]= useNewPostMutation()
+
+
+const submit=(e)=>{
+  e.preventDefault();
+  newPost({title: title , body : body,id:Math.random*1000,userId:Math.random*1000})
+  setBody("")
+  setTitle("")
+}
+
   return (
     <>
      <div ><h1>my Post</h1>
+     <div>
+< form onSubmit={submit}>
+<input
+type='text'
+placeholder='Enter Title'
+value={title}
+onChange={e=>setTitle(e.target.value)}
+></input>
+<input
+type='text'
+placeholder='Enter Body'
+value={body}
+onChange={e=>setBody(e.target.value)}
+></input>
+<button type='submit'>Add</button>
+</form>
+
+     </div>
      {data?.map(item=>{
    return   (<PostItemList key={item.id} title={item.title} body={item.body}/>)
      })}
--- src/redux/api.js
+++ src/redux/api.js
@@ -1,11 +1,19 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const myAPI=createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({
-        baseUrl:"http://localhost:3000/"
-    }),
-    endpoints:(builder)=>({
-        getPosts:builder.query({query:()=>"posts"})
-})})
-export const{useGetPostsQuery}=myAPI
+export const myAPI = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({ query: () => "posts" }),
+    newPost:builder.mutation({
+        query:(post)=>({
+            url:"posts",
+            method:"POST",
+            body:post,
+        })
+    })
+  }),
+});
+export const { useGetPostsQuery,useNewPostMutation } = myAPI;
